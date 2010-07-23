a3ad9b0084f49e0ca921792a26135921f1624a46
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-23 12:09:39 +0000

    Last time I said it works? I was kidding. Try this.

--- aais/aais-server/Server.fs
+++ aais/aais-server/Server.fs
@@ -21,62 +21,46 @@ let cache = new CacheService()
 let cacheServiceToken = new CancellationTokenSource()
 let cacheService = async {
     while not cacheServiceToken.IsCancellationRequested do
-        try
-            listener.Start 10
-            try
-                let socket = listener.AcceptTcpClient()
-                let cacheServiceHandler(socket: TcpClient) = async {
-                    try
-                        let reader = new StreamReader(socket.GetStream())
-                        let writer = new StreamWriter(socket.GetStream())
-                        let ASCII = new ASCIIEncoding()
-                        let command = reader.ReadLine()
-                        match command with
-                        | "STORE" ->
-                            let value = List.ofArray(ASCII.GetBytes(reader.ReadLine().ToCharArray()))
-                            let key = cache.store value
-                            writer.WriteLine(key.ToString())
-                            writer.Flush()
-                        | "SEARCH" ->
-                            let key = Int32.Parse(reader.ReadLine())
-                            let value = cache.search key
-                            writer.WriteLine(ASCII.GetString(List.toArray value))
-                            writer.Flush()
-                        | "REMOVE" ->
-                            let key = Int32.Parse(reader.ReadLine())
-                            cache.remove key
-                        // TODO: avoid failing with an error
-                        | _ -> failwith "command not found"
-                        socket.Close()
-                    with
-                    | :? ObjectDisposedException as e -> Console.WriteLine e.Message
-                    | :? IOException as e -> Console.WriteLine e.Message
-                    | :? OverflowException as e -> Console.WriteLine e.Message
-                }
-                Async.Start(cacheServiceHandler socket)
-            with
-            | :? InvalidOperationException as e ->
-                Console.WriteLine e.Message
-                raise(new SocketException())
-        finally
-            listener.Stop()
-}
+        listener.Start 10
+        use socket = listener.AcceptTcpClient()
+        
+        let cacheServiceHandler(socket: TcpClient) = async {
+            use reader = new StreamReader(socket.GetStream())
+            use writer = new StreamWriter(socket.GetStream())
+            let ASCII = new ASCIIEncoding()
+            let command = reader.ReadLine()
+            match command with
+            | "STORE" ->
+                let value = List.ofArray(ASCII.GetBytes(reader.ReadLine().ToCharArray()))
+                let key = cache.store value
+                writer.WriteLine(key.ToString())
+                writer.Flush()
+            | "SEARCH" ->
+                let key = Int32.Parse(reader.ReadLine())
+                let value = cache.search key
+                writer.WriteLine(ASCII.GetString(List.toArray value))
+                writer.Flush()
+            | "REMOVE" ->
+                let key = Int32.Parse(reader.ReadLine())
+                cache.remove key
+            // TODO: avoid failing with an error
+            | _ -> failwith "command not found"}
+                
+        Async.Start(cacheServiceHandler socket)}
 
 Async.Start(cacheService, cacheServiceToken.Token)
 let outbox = cache.getMailbox
 while true do
-    try
-        Console.Write "> "
-        let line = Console.ReadLine()
-        let command = line.Split([|' '|]).[0]
-        let parameter = Int32.Parse(line.Split([|' '|]).[1])
-        match command with
-        | "size" ->
-            outbox.Post(Msg(command, parameter))
-            cache.update
-        | "log" ->
-            outbox.Post(Msg(command, parameter))
-        // TODO: avoid failing with an error
-        | _ -> failwith "command not found"
-    with
-    | :? OverflowException as e -> Console.WriteLine e.Message
+    Console.Write "> "
+    let line = Console.ReadLine()
+    let command = line.Split([|' '|]).[0]
+    let parameter = Int32.Parse(line.Split([|' '|]).[1])
+    match command with
+    | "size" ->
+        outbox.Post(Msg(command, parameter))
+        cache.update
+    | "log" ->
+        outbox.Post(Msg(command, parameter))
+    // TODO: avoid failing with an error
+    | _ -> failwith "command not found"
+    
