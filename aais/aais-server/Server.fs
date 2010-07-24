72c648ea962af9c239a3a24f4fdb3411916367a2
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-24 12:32:13 +0000

    Last time I said it works? I was kidding. Try this.

--- aais/aais-server/Server.fs
+++ aais/aais-server/Server.fs
@@ -1,8 +1,16 @@
 ﻿#light
 
-module Application.Server
+module Application.Server.Main
+
+open System
 
 open Application.Server.CacheService
 
 let cache = new CacheService()
 let key = cache.store [for i in 0 .. 9 -> byte i]
+let value = cache.search key
+printfn "%A" value
+
+cache.search 2 |> ignore
+
+Console.ReadKey() |> ignore
