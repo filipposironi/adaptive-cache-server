7dbccbde8339e96abe6d67c5d39a7f1eff92f992
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-24 07:38:34 +0000

    CacheService.fs contains a basic version of the cache service written in a fully functional fashion Server.fs contains a basic example using the cache service

--- trunk/aais/aais-server/CacheService.fs
+++ trunk/aais/aais-server/CacheService.fs
@@ -1,108 +1,37 @@
 ﻿#light
 
-module CacheService
+module Application.Server.CacheService
 
 open System
-open System.Collections
-open System.Collections.Generic
-open System.IO
-open System.Runtime.Serialization
-open System.Runtime.Serialization.Formatters.Binary
 
-type Message = Msg of string * int
-
-type ICacheService =
-    abstract member store: byte list -> int
-    abstract member search: int -> byte list
-    abstract member remove: int -> unit
-
+type internal Message = Store of byte list * AsyncReplyChannel<int>
+                      | Remove of int
+                      | Search of int * AsyncReplyChannel<byte list>
+                      | Size of int
+                      | Log of bool
 type CacheService() =
-    let cacheMaxSize = Convert.ToInt32 UInt16.MaxValue
-    let enableLogMessages = ref false
-    let keys = ref(Map.ofList [for i in 0 .. cacheMaxSize -> (i, true)])
-    let volatileCache = ref(new Map<int, byte list>([]))
-    let volatileCacheSize = ref 0
-    let volatileCacheMaxSize = ref 1024
-    let inbox = new MailboxProcessor<Message>(fun (inbox: MailboxProcessor<Message>) -> async {
-        while true do
-            let! message = inbox.Receive()
-            match message with
-            | Msg("size", size) ->
-                if size <= 0 then
-                    // TODO: avoid failing with an error
-                    failwith "size less than or equal to zero"
-                else
-                    volatileCacheMaxSize := size
-            | Msg("log", enable) ->
-                if enable = 0 then
-                    enableLogMessages := false
-                else
-                    enableLogMessages := true
-            | Msg(_, _) -> ()
-    })
-    
-    do
-        // TODO: open log file
-        inbox.Start()
-       
-    member this.getMailbox = inbox
+    let MessageService = MailboxProcessor.Start(fun inbox ->
+        let rec MessageHandler (cache: Map<int, byte list>) (keys: Map<int, bool>) =
+            async { let! message = inbox.Receive()
+                    match message with
+                    | Store(value, outbox) ->
+                        let key = Map.findKey (fun k v -> v = true) keys
+                        outbox.Reply key
+                        return! MessageHandler (Map.add key value cache) (Map.add key false keys)
+                    | Remove(key) ->
+                        return! MessageHandler (Map.remove key cache) (Map.add key true keys)
+                    | Search(key, outbox) ->
+                        let value = Map.find key cache
+                        outbox.Reply value
+                        return! MessageHandler cache keys
+                    | Size(size) ->
+                        // TODO
+                        return! MessageHandler cache keys
+                    | Log(log) ->
+                        // TODO
+                        return! MessageHandler cache keys }
+        MessageHandler (new Map<int, byte list>([])) (Map.ofList [for i in 0 .. (Convert.ToInt32 UInt16.MaxValue) -> (i, true)]))
 
-    member this.store = (this :> ICacheService).store
-    member this.search = (this :> ICacheService).search
-    member this.remove = (this :> ICacheService).remove
-    
-    member this.update =
-        if volatileCacheSize > volatileCacheMaxSize then
-            let size = ref 0
-            for (k, v) in Map.toSeq !volatileCache do
-                if !size + v.Length > !volatileCacheMaxSize then
-                    let file = new FileStream(k.ToString() + ".dat", FileMode.Create)
-                    let formatter = new BinaryFormatter()
-                    formatter.Serialize(file, v)
-                    file.Close()
-                    volatileCache := Map.add k [] !volatileCache
-                    volatileCacheSize := !volatileCacheSize - v.Length
-                else
-                    size := !size + v.Length
-
-    interface ICacheService with
-        member this.store(value: byte list) =
-            let key = Map.findKey (fun k v -> v = true) !keys
-            keys := Map.add key false !keys
-            if !volatileCacheSize + value.Length <= !volatileCacheMaxSize then
-                volatileCache := Map.add key value !volatileCache
-                volatileCacheSize := !volatileCacheSize + value.Length
-            else
-                volatileCache := Map.add key [] !volatileCache
-                let file = new FileStream(key.ToString() + ".dat", FileMode.Create)
-                let formatter = new BinaryFormatter()
-                formatter.Serialize(file, value)
-                file.Close()
-            key
-        
-        member this.search(key: int) =
-            try
-                if Map.find key !volatileCache = [] then
-                    let file = new FileStream(key.ToString() + ".dat", FileMode.Open)
-                    let formatter = new BinaryFormatter()
-                    let value =(formatter.Deserialize(file) :?> byte list)
-                    file.Close()
-                    value
-                else
-                    Map.find key !volatileCache
-            with
-            | :? KeyNotFoundException as e ->
-                Console.WriteLine e.Message
-                []
-
-        member this.remove(key: int) =
-            try
-                let value = Map.find key !volatileCache
-                if value.Length = 0 then
-                    File.Delete(key.ToString() + ".dat")
-                else
-                    volatileCacheSize := !volatileCacheSize - value.Length
-            with
-                | :? KeyNotFoundException as e -> Console.WriteLine e.Message
-            volatileCache := Map.remove key !volatileCache
-            keys := Map.add key true !keys
+    member this.store value = MessageService.PostAndReply(fun inbox -> Store(value, inbox))
+    member this.remove key = MessageService.Post(Remove(key))
+    member this.search key = MessageService.PostAndReply(fun inbox -> Search(key, inbox))
