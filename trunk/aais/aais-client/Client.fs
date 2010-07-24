0fea485642a38e2f3d589a1d38ff801d9e27bde6
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-24 12:32:13 +0000

    Last time I said it works? I was kidding. Try this.

--- trunk/aais/aais-client/Client.fs
+++ trunk/aais/aais-client/Client.fs
@@ -1,3 +1,7 @@
 ﻿#light
 
 module Application.Client
+
+open System
+
+Console.WriteLine "Adaptive Cache Service - Client"
--- trunk/aais/aais-server/CacheService.fs
+++ trunk/aais/aais-server/CacheService.fs
@@ -3,34 +3,86 @@
 module Application.Server.CacheService
 
 open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Runtime.Serialization
+open System.Runtime.Serialization.Formatters.Binary
 
-type internal Message = Store of byte list * AsyncReplyChannel<int>
+let private logSource = "AdaptiveCacheService"
+let private logName = "Application"
+
+let private initialCache = new Map<int, bool * byte list>([])
+let private initialCacheKeys = Map.ofList [for i in 0 .. (Convert.ToInt32 UInt16.MaxValue) -> (i, true)]
+let private initialVolatileCacheSize = 0
+let private initialVolatileCacheMaxSize = 1024
+
+type private Message = Store of byte list * AsyncReplyChannel<int>
                       | Remove of int
                       | Search of int * AsyncReplyChannel<byte list>
                       | Size of int
                       | Log of bool
+
 type CacheService() =
     let MessageService = MailboxProcessor.Start(fun inbox ->
-        let rec MessageHandler (cache: Map<int, byte list>) (keys: Map<int, bool>) =
+        let rec MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize =
             async { let! message = inbox.Receive()
                     match message with
                     | Store(value, outbox) ->
-                        let key = Map.findKey (fun k v -> v = true) keys
-                        outbox.Reply key
-                        return! MessageHandler (Map.add key value cache) (Map.add key false keys)
+                        let key = Map.findKey (fun k v -> v = true) cacheKeys
+                        let cacheKeys = Map.add key false cacheKeys
+                        if volatileCacheSize + value.Length <= volatileCacheMaxSize then
+                            let cache = Map.add key (true, value) cache
+                            let volatileCacheSize = volatileCacheSize + value.Length
+                            outbox.Reply key
+                            return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
+                        else
+                            let cache = Map.add key (false, []) cache
+                            use file = new FileStream(key.ToString() + ".dat", FileMode.Create)
+                            let formatter = new BinaryFormatter()
+                            formatter.Serialize(file, box value)
+                            outbox.Reply key
+                            return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
                     | Remove(key) ->
-                        return! MessageHandler (Map.remove key cache) (Map.add key true keys)
+                        if Map.containsKey key cache then
+                            let (isVolatile, value) = Map.find key cache
+                            if isVolatile then
+                                let volatileCacheSize = volatileCacheSize - value.Length
+                                let cache = Map.remove key cache
+                                let cacheKeys = Map.add key true cacheKeys
+                                return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
+                            else
+                                File.Delete(key.ToString() + ".dat")
+                                let cache = Map.remove key cache
+                                let cacheKeys = Map.add key true cacheKeys
+                                return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
+                        else
+                            return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
                     | Search(key, outbox) ->
-                        let value = Map.find key cache
-                        outbox.Reply value
-                        return! MessageHandler cache keys
-                    | Size(size) ->
+                        try
+                            let (isVolatile, value) = Map.find key cache
+                            if isVolatile then
+                                outbox.Reply value
+                            else
+                                use file = new FileStream(key.ToString() + ".dat", FileMode.Open)
+                                let formatter = new BinaryFormatter()
+                                let value = unbox<byte list> (formatter.Deserialize(file))
+                                outbox.Reply value
+                        with
+                        | :? KeyNotFoundException as e ->
+                            if not (EventLog.SourceExists(logSource)) then
+                                EventLog.CreateEventSource(logSource, logName)
+                            EventLog.WriteEntry(logSource, e.Message, EventLogEntryType.Warning)
+                            outbox.Reply []
+                        return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize
+                    | Size(newVolatileCacheSize) ->
                         // TODO
-                        return! MessageHandler cache keys
+                        return! MessageHandler cache cacheKeys newVolatileCacheSize volatileCacheMaxSize
                     | Log(log) ->
                         // TODO
-                        return! MessageHandler cache keys }
-        MessageHandler (new Map<int, byte list>([])) (Map.ofList [for i in 0 .. (Convert.ToInt32 UInt16.MaxValue) -> (i, true)]))
+                        return! MessageHandler cache cacheKeys volatileCacheSize volatileCacheMaxSize}
+        MessageHandler initialCache initialCacheKeys initialVolatileCacheSize initialVolatileCacheMaxSize)
 
     member this.store value = MessageService.PostAndReply(fun inbox -> Store(value, inbox))
     member this.remove key = MessageService.Post(Remove(key))
