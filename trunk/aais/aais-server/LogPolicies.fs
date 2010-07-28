943dd544db4287432218050f200a947eb72096f2
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-28 14:48:41 +0000

    Added LogPolicies.fs

---
+++ trunk/aais/aais-server/LogPolicies.fs
@@ -0,0 +1,40 @@
+﻿#light
+
+module LogPolicies
+
+open System.Diagnostics
+
+open Helpers
+
+type LogPolicy =
+    abstract log: string -> (string * LogLevel) list -> unit
+
+type InformationLogPolicy() =
+    member this.log = (this :> LogPolicy).log
+    
+    interface LogPolicy with
+        member this.log source messages =
+            for (message, level) in messages do
+                match level with
+                | Information ->
+                    writeLogEntry source message level
+                | _ -> ()
+
+type WarningLogPolicy() =
+    member this.log = (this :> LogPolicy).log
+    
+    interface LogPolicy with
+        member this.log source messages =
+            for (message, level) in messages do
+                match level with
+                | Information | Warning ->
+                    writeLogEntry source message level
+                | _ -> ()
+
+type ErrorLogPolicy() =
+    member this.log = (this :> LogPolicy).log
+    
+    interface LogPolicy with
+        member this.log source messages =
+            for (message, level) in messages do
+                writeLogEntry source message level
