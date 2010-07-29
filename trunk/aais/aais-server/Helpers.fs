b1ae2bcd2674ffb1a23001f35f65f9916ec3490a
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-29 16:48:37 +0000

    * Helpers re-factored * LogPolicies completed using the factory design pattern * MemoryPolicies completed * CacheService completely re-written * Server modified to reflect changes in CacheService

--- trunk/aais/aais-server/Helpers.fs
+++ trunk/aais/aais-server/Helpers.fs
@@ -2,43 +2,7 @@
 
 module Helpers
 
-open System
-open System.Diagnostics
-open System.IO
-open System.Runtime.Serialization
-open System.Runtime.Serialization.Formatters.Binary
 open System.Text.RegularExpressions
-
-type LogLevel = Information
-              | Warning
-              | Error
-
-let writeLogEntry source message level =
-    let name = "Application"
-    if not (EventLog.SourceExists(source)) then
-        EventLog.CreateEventSource(source, name)
-    match level with
-    | Information ->
-        EventLog.WriteEntry(source, message, EventLogEntryType.Information)        
-    | Warning ->
-        EventLog.WriteEntry(source, message, EventLogEntryType.Warning)
-        Console.WriteLine(message)
-    | Error ->
-        EventLog.WriteEntry(source, message, EventLogEntryType.Error)
-        Console.WriteLine(message)
-
-let serializeCacheLine key value =
-    use file = new FileStream(key.ToString() + ".dat", FileMode.Create)
-    let formatter = new BinaryFormatter()
-    formatter.Serialize(file, value)
-    file.Close()
-
-let deserializeCacheLine key =
-    use file = new FileStream(key.ToString() + ".dat", FileMode.Open)
-    let formatter = new BinaryFormatter()
-    let value = formatter.Deserialize(file)
-    file.Close()
-    value
 
 let (|ParseRegex|_|) regex string =
     let matched = Regex(regex).Match(string)
--- trunk/aais/aais-server/LogPolicies.fs
+++ trunk/aais/aais-server/LogPolicies.fs
@@ -2,9 +2,29 @@
 
 module LogPolicies
 
+open System
 open System.Diagnostics
 
 open Helpers
+
+type LogLevel = Information
+              | Warning
+              | Error
+
+let writeLogEntry source message level =
+    let name = "Application"
+    if not (EventLog.SourceExists(source)) then
+        EventLog.CreateEventSource(source, name)
+    match level with
+    | Information ->
+        EventLog.WriteEntry(source, message, EventLogEntryType.Information)        
+    | Warning ->
+        EventLog.WriteEntry(source, message, EventLogEntryType.Warning)
+        Console.WriteLine(message)
+    | Error ->
+        EventLog.WriteEntry(source, message, EventLogEntryType.Error)
+        Console.WriteLine(message)
+
 
 type LogPolicy =
     abstract log: string -> (string * LogLevel) list -> unit
@@ -15,10 +35,7 @@ type InformationLogPolicy() =
     interface LogPolicy with
         member this.log source messages =
             for (message, level) in messages do
-                match level with
-                | Information ->
-                    writeLogEntry source message level
-                | _ -> ()
+                writeLogEntry source message level
 
 type WarningLogPolicy() =
     member this.log = (this :> LogPolicy).log
@@ -27,7 +44,7 @@ type WarningLogPolicy() =
         member this.log source messages =
             for (message, level) in messages do
                 match level with
-                | Information | Warning ->
+                | Warning | Error ->
                     writeLogEntry source message level
                 | _ -> ()
 
@@ -37,4 +54,13 @@ type ErrorLogPolicy() =
     interface LogPolicy with
         member this.log source messages =
             for (message, level) in messages do
-                writeLogEntry source message level
+                match level with
+                | Error -> writeLogEntry source message level
+                | _ -> ()
+
+type FactoryLogPolicy() =
+    static member create level =
+        match level with
+        | Information -> (new InformationLogPolicy() :> LogPolicy)
+        | Warning -> (new WarningLogPolicy() :> LogPolicy)
+        | Error -> (new ErrorLogPolicy() :> LogPolicy)
