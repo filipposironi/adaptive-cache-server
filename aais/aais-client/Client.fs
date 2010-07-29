7386e1a7a17fcc36c91059fe2dd3f8162073550b
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-07-29 17:44:00 +0000

    Last time I said it works? I was kidding. Try this.

--- aais/aais-client/Client.fs
+++ aais/aais-client/Client.fs
@@ -10,14 +10,43 @@ open System.Text
 
 let private address = "127.0.0.1"
 let private port = 1234
-let private client = new TcpClient(address, port)
-let private reader = new StreamReader(client.GetStream())
-let private writer = new StreamWriter(client.GetStream())
 
-let strings = ["pera"; "mela"; "fragola"; "banana"; "albicocca"]
-for string in strings do
-    writer.WriteLine("store")
-    writer.Flush()
-    writer.WriteLine(string)
-    writer.Flush()
-    Console.WriteLine(reader.ReadLine())
+let store words =
+    let rec loop words (keys: int list) =
+        match words with
+        | [] -> keys
+        | (head :: tail) ->
+            use client = new TcpClient(address, port)
+            use reader = new StreamReader(client.GetStream())
+            use writer = new StreamWriter(client.GetStream())
+            writer.WriteLine("store " + head)
+            writer.Flush()
+            let keys = keys @ [Int32.Parse(reader.ReadLine())]
+            loop tail keys
+
+    loop words []
+
+let keys = store ["pera"; "mela"; "fragola"; "banana"; "albicocca"]
+for k in keys do
+    Console.WriteLine(k)
+
+let search keys =
+    let rec loop keys (words: string list) =
+        match keys with
+        | [] -> words
+        | (head :: tail) ->
+            use client = new TcpClient(address, port)
+            use reader = new StreamReader(client.GetStream())
+            use writer = new StreamWriter(client.GetStream())
+            writer.WriteLine("search " + head.ToString())
+            writer.Flush()
+            let words = words @ [reader.ReadLine()]
+            loop tail words
+
+    loop keys []
+
+let words = search keys
+for w in words do
+    Console.WriteLine(w)
+
+Console.ReadKey() |> ignore
