7046f51913998f52dd8fe1edf35a410e6eb548e1
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-08-01 19:33:35 +0000

    * Fixed a key handling bug to manage empty key set * Changed the data structure used to manage available keys from a Map to a Stack * Updated network communication protocol * Updated client to reflect changes in server and the network communication protocol

--- trunk/aais/aais-server/CacheService.fs
+++ trunk/aais/aais-server/CacheService.fs
@@ -19,40 +19,49 @@ type private Message = Store of byte list * AsyncReplyChannel<int>
 
 type CacheService() =
     let source = "AdaptiveCacheService"
+    let timeout = 1000
     let messageService = MailboxProcessor.Start(fun inbox ->
-        let rec loop cache cacheKeys volatileCacheSize (memoryPolicy: MemoryPolicy) (logPolicy: LogPolicy) = async {
+        let rec loop cache keys volatileCacheSize (memoryPolicy: MemoryPolicy) (logPolicy: LogPolicy) = async {
             let! message = inbox.Receive()
             match message with
             | Store(value, outbox) ->
-                let key = Map.findKey (fun key value -> value = true) cacheKeys
-                let cacheKeys = Map.add key false cacheKeys
-                let (cache, volatileCacheSize, log) = memoryPolicy.store key value cache volatileCacheSize
-                logPolicy.log source log
-                outbox.Reply key
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                match keys with
+                | [] ->
+                    let log = [("Key not available.", Warning)]
+                    logPolicy.log source log
+                    return! loop cache keys volatileCacheSize memoryPolicy logPolicy
+                | key :: keys ->
+                    let (cache, volatileCacheSize, log) = memoryPolicy.store key value cache volatileCacheSize
+                    logPolicy.log source log
+                    outbox.Reply key
+                    return! loop cache keys volatileCacheSize memoryPolicy logPolicy
             | Remove(key) ->
                 let (cache, volatileCacheSize, log) = memoryPolicy.remove key cache volatileCacheSize
                 logPolicy.log source log
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                return! loop cache (key :: keys) volatileCacheSize memoryPolicy logPolicy
             | Search(key, outbox) ->
-                let (value, log) = memoryPolicy.search key cache 
-                logPolicy.log source log
-                outbox.Reply value
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                match memoryPolicy.search key cache with
+                | ([], log) ->
+                    logPolicy.log source log
+                    return! loop cache keys volatileCacheSize memoryPolicy logPolicy
+                | (value, log) ->
+                    logPolicy.log source log
+                    outbox.Reply value
+                    return! loop cache keys volatileCacheSize memoryPolicy logPolicy
             | LowMemory(volatileCacheMaxSize) ->
                 let oldVolatileCacheMaxSize = memoryPolicy.size
                 let memoryPolicy = new LowMemoryPolicy(volatileCacheMaxSize)
                 let (cache, volatileCacheSize) = memoryPolicy.update cache volatileCacheSize oldVolatileCacheMaxSize
                 let log = [("Memory context changed to \"Low Availability\".", Information); ("Memory bound set to \"" + volatileCacheMaxSize.ToString() + "\".", Information)]
                 logPolicy.log source log
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                return! loop cache keys volatileCacheSize memoryPolicy logPolicy
             | HighMemory ->
                 let oldVolatileCacheMaxSize = memoryPolicy.size
                 let memoryPolicy = new HighMemoryPolicy()
                 let (cache, volatileCacheSize) = memoryPolicy.update cache volatileCacheSize oldVolatileCacheMaxSize
                 let log = [("Memory context changed to \"High Availability\".", Information)]
                 logPolicy.log source log
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                return! loop cache keys volatileCacheSize memoryPolicy logPolicy
             | Log(level) ->
                 let logPolicy = FactoryLogPolicy.create level
                 match level with
@@ -65,15 +74,15 @@ type CacheService() =
                 | Error ->
                     let log = [("Log context changed to \"Error\".", Information)]
                     logPolicy.log source log
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy
+                return! loop cache keys volatileCacheSize memoryPolicy logPolicy
             | Config(outbox) ->
                 outbox.Reply [memoryPolicy.ToString(); logPolicy.ToString()]
-                return! loop cache cacheKeys volatileCacheSize memoryPolicy logPolicy}
-        loop (new Map<int, bool * int * byte list>([])) (Map.ofList [for i in 0 .. (Convert.ToInt32 UInt16.MaxValue) -> (i, true)]) 0 (new HighMemoryPolicy()) (FactoryLogPolicy.create Warning))
+                return! loop cache keys volatileCacheSize memoryPolicy logPolicy}
+        loop (new Map<int, bool * int * byte list>([])) [for i in 0 .. (*(Convert.ToInt32 UInt16.MaxValue)*) 1 -> i] 0 (new HighMemoryPolicy()) (FactoryLogPolicy.create Warning))
 
-    member this.store value = messageService.PostAndReply(fun inbox -> Store(value, inbox))
+    member this.store value = messageService.PostAndTryAsyncReply((fun inbox -> Store(value, inbox)), timeout)
     member this.remove key = messageService.Post(Remove(key))
-    member this.search key = messageService.PostAndReply(fun inbox -> Search(key, inbox))
+    member this.search key = messageService.PostAndTryAsyncReply((fun inbox -> Search(key, inbox)), timeout)
     member this.low size = messageService.Post(LowMemory(size))
     member this.high = messageService.Post(HighMemory)
     member this.log level = messageService.Post(Log(level))
