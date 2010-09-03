9b61d43f4c6ae0002fd1259996bdf3be59544594
filippo.sironi <filippo.sironi@e9443143-b3d7-0d92-fc57-214081b2d7ba>
2010-09-03 18:27:48 +0000

    * Updated license * Deleted LogPolicies.fs

--- aais/aais-server/error/Log.fs
+++ aais/aais-server/error/Log.fs
@@ -1,12 +1,12 @@
-﻿(* 
+﻿(*
  * Copyright (c) 2010
  * Filippo Sironi (filippo.sironi@gmail.com)
  * Matteo Villa (villa.matteo@gmail.com)
  * ----------------------------------------------------------------------------
  *                        "THE BEER-WARE LICENSE"
  * Filippo Sironi and Matteo Villa wrote this file. As long as you retain this
- * notice you can do whatever you want with this stuff. If we meet some day, and
- * you think this stuff is worth it, you can buy us a beer in return.
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy us a beer in return.
  * ----------------------------------------------------------------------------
  *)
 
